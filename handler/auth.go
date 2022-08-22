25f26be114bcd644e91434fdb97db2b004985f3b
Chris <chris@hopfen.space>
2022-08-23 00:08:56 +0200

    Updated the authentication endpoints to use the new utility models

--- handler/auth.go
+++ handler/auth.go
@@ -10,32 +10,34 @@ import (
 	"github.com/myOmikron/echotools/utilitymodels"
 )
 
-type TestRequest struct {
-	Authenticated bool `json:"authenticated"`
+type TestResponse struct {
+	Error         bool   `json:"error"`
+	Message       string `json:"message"`
+	Authenticated bool   `json:"authenticated" json:"authenticated,omitempty"`
 }
 
-func (a *Api) Test(c echo.Context) error {
+func (a *API) Test(c echo.Context) error {
 	if context, err := middleware.GetSessionContext(c); err != nil {
-		return c.String(500, "")
+		return c.JSON(500, TestResponse{Authenticated: false, Error: true, Message: "Session error"})
 	} else {
-		return c.JSON(200, TestRequest{Authenticated: context.IsAuthenticated()})
+		return c.JSON(200, TestResponse{Authenticated: context.IsAuthenticated(), Error: false, Message: "Successfully authenticated"})
 	}
 }
 
-func (a *Api) Logout(c echo.Context) error {
+func (a *API) Logout(c echo.Context) error {
 	if context, err := middleware.GetSessionContext(c); err != nil {
-		return c.String(500, "")
+		return c.JSON(500, GenericResponse{Error: true, Message: "Invalid session"})
 	} else {
 		if context.IsAuthenticated() {
 			if err := middleware.Logout(a.DB, c); err != nil {
 				if errors.Is(err, echo.ErrCookieNotFound) {
-					return c.String(200, "")
+					return c.JSON(200, GenericResponse{Error: false, Message: "Successfully logged out"})
 				} else {
-					return c.JSON(500, ErrorReturn{Error: "Database Error"})
+					return c.JSON(500, GenericResponse{Error: true, Message: "Database Error"})
 				}
 			}
 		}
-		return c.String(200, "")
+		return c.JSON(200, GenericResponse{Error: false, Message: "Successfully logged out"})
 	}
 }
 
@@ -44,25 +46,20 @@ type LoginRequest struct {
 	Password *string `json:"password" echotools:"required;not empty"`
 }
 
-type ErrorReturn struct {
-	Error string `json:"error"`
-}
-
-func (a *Api) Login(c echo.Context) error {
-	var f LoginRequest
-	if err := utility.ValidateJsonForm(c, &f); err != nil {
-		return c.JSON(400, ErrorReturn{Error: err.Error()})
+func (a *API) Login(c echo.Context) error {
+	var r LoginRequest
+	if err := utility.ValidateJsonForm(c, &r); err != nil {
+		return c.JSON(400, GenericResponse{Error: true, Message: err.Error()})
 	}
 
-	if user, err := auth.Authenticate(a.DB, *f.Username, *f.Password); err != nil {
-		return c.String(401, "")
+	if user, err := auth.AuthenticateLocalUser(a.DB, *r.Username, *r.Password); err != nil {
+		return c.JSON(401, GenericResponse{Error: true, Message: "Invalid username or password"})
 	} else {
-		if err := middleware.Login(a.DB, user, c); err != nil {
-			return c.JSON(500, ErrorReturn{Error: err.Error()})
+		if err := middleware.Login(a.DB, user, c, true); err != nil {
+			return c.JSON(500, GenericResponse{Error: true, Message: err.Error()})
 		}
 	}
-
-	return c.String(200, "")
+	return c.JSON(200, GenericResponse{Error: false, Message: "Successfully logged in"})
 }
 
 type RegisterRequest struct {
@@ -70,30 +67,36 @@ type RegisterRequest struct {
 	Password *string `json:"password" echotools:"required;not empty"`
 }
 
-func (a *Api) Register(c echo.Context) error {
-	var f RegisterRequest
-	if err := utility.ValidateJsonForm(c, &f); err != nil {
-		return c.JSON(400, ErrorReturn{Error: err.Error()})
+func (a *API) Register(c echo.Context) error {
+	var r RegisterRequest
+	if err := utility.ValidateJsonForm(c, &r); err != nil {
+		return c.JSON(400, GenericResponse{Error: true, Message: err.Error()})
 	}
 
 	var userCount int64
-	if err := a.DB.Find(&utilitymodels.User{}, "username = ?", *f.Username).Count(&userCount).Error; err != nil {
+	if err := a.DB.Find(&utilitymodels.LocalUser{}, "username = ?", *r.Username).Count(&userCount).Error; err != nil {
 		c.Logger().Error(err)
-		return c.JSON(500, ErrorReturn{Error: "Database error"})
+		return c.JSON(500, GenericResponse{Error: true, Message: "Database error"})
 	}
 
 	if userCount != 0 {
-		return c.JSON(409, ErrorReturn{Error: "User with that username already exists"})
+		return c.JSON(409, GenericResponse{Error: true, Message: "User with that username already exists"})
 	}
 
-	if _, err := database.CreateUser(a.DB, *f.Username, *f.Password, nil, true); err != nil {
-		return c.JSON(500, ErrorReturn{Error: err.Error()})
+	if _, err := database.CreateLocalUser(a.DB, *r.Username, *r.Password, nil); err != nil {
+		return c.JSON(500, GenericResponse{Error: true, Message: err.Error()})
 	} else {
-		return c.String(201, "")
+		return c.JSON(201, GenericResponse{Error: false, Message: "Successfully registered new account"})
 	}
 }
 
+type ConnectRequest struct {
+	Username    *string `json:"username" echotools:"required;not empty"`
+	Password    *string `json:"password" echotools:"required;not empty"`
+	Alias       *string `json:"alias" echotools:"required;not empty"`
+	Application *string `json:"application" echotools:"required;not empty"`
+}
+
 func (a *API) Connect(c echo.Context) error {
-	_ = c // TODO
 	return nil
 }
