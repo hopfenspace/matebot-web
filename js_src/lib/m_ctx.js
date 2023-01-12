4017ba8e41add02fb1d7933884cdc0848026c174
myOmikron <git@omikron.dev>
2023-01-12 02:29:21 +0100

    Removed old frontend

--- js_src/lib/m_ctx.js
+++
@@ -1,6 +0,0 @@
-import {React} from "../m_react.js";
-import SDK from "./m_sdk.js";
-
-let sdk = new SDK();
-let ctx = React.createContext({"static": "/static/", "sdk": sdk});
-export default ctx;
--- start_jsx_transpiler.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-npx babel js_src --out-dir static/js --presets react-app/prod --minified --watch --ignore=m_sdk.js
